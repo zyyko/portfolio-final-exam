0dddbc6f58465aaebb2332a499d176caa10113c3
zyko <zyko.taouaf@gmail.com>
2025-01-15 19:39:21 +0100

    done

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const mysql = require("mysql");
 
 // Create and export the database connection
 const con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "cv",
+  host: "bar6udqr8df8ya71r0hd-mysql.services.clever-cloud.com",
+  user: "uge55g2m6uac0i6v",
+  password: "ZDo21clF4BlA7j4TWgt2",
+  database: "bar6udqr8df8ya71r0hd",
 });
 
 con.connect(function (error) {
